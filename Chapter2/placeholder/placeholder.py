a7519637aa9b869e937d418a56a0361e7cfab272
Dan Wood <danielalexwood@gmail.com>
2015-06-26 13:18:24 -0700

    added basic image generation

--- Chapter2/placeholder/placeholder.py
+++ Chapter2/placeholder/placeholder.py
@@ -28,6 +28,8 @@ from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
 from django import forms
 from django.conf.urls import url
+from io import BytesIO
+from PIL import Image
 
 
 class ImageForm(forms.Form):
@@ -36,14 +38,22 @@ class ImageForm(forms.Form):
     height = forms.IntegerField(min_value=1, max_value=2000)
     width = forms.IntegerField(min_value=1, max_value=2000)
 
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes"""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
 
 def placeholder(request, width, height):
     form = ImageForm({'height': height, 'width': width})
     if form.is_valid():
-        height = form.cleaned_data['height']
-        width = form.cleaned_data['width']
-        # TODO: Generate image of requested size
-        return HttpResponse('Ok')
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
     else:
         return HttpResponseBadRequest('Invalid image request')
 
