5f8d6793a7950fd14a9718a8d7970355eef81975
Dan Wood <danielalexwood@gmail.com>
2015-06-27 18:36:49 -0700

    root reorganization

---
+++ static_site_generator/prototype.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='(p#fhebimg91@m!8mdtp)erua@d=ef(18#t9*t!0e-1!_n124s',
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'django.contrib.webdesign',
+        'sitebuilder',
+    ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages')
+)
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
