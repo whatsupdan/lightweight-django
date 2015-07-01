fb4a62db274b61d82afd358bd41b4e94cb9cbd5c
Dan Wood <danielalexwood@gmail.com>
2015-07-01 04:19:57 -0700

    base structure for backbone SPA works

--- scrum-repo/scrum/scrum/urls.py
+++ scrum-repo/scrum/scrum/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -7,4 +8,5 @@ from board.urls import router
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='board/index.html')),
 ]
